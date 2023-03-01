58de0c2011eeb3a9bc39ff27e276f831a287f784
Matheus do Prado <32469329+theprado@users.noreply.github.com>
2023-03-01 11:36:25 -0300

    removed mantine dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import  { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
-import { MantineProvider, Text } from '@mantine/core';
-import { DatePicker, Calendar, Month } from '@mantine/dates';
-
-
-
-
-
 
 function App() {
   const session = useSession();  // tokens, when session exists we have a user
@@ -45,12 +38,7 @@ console.log(session);
         <>
           <h2>Oi {session.user.email}</h2>
           <p>Começo do evento</p>
-<<<<<<< HEAD
           
-          
-=======
-          <DatePicker placeholder="Escolher data" withAsterisk />
->>>>>>> main
           <button onClick={() => signOut()}>Sair</button>
         </>
         :
